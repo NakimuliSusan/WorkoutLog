2ea79da2aca69e6c7ed320f5b3bd2af0b24950a6
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-06-04 08:18:19 +0300

    splash-screen changes

--- app/src/main/java/dev/pinky/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/pinky/workoutlog/SplashActivity.kt
@@ -1,11 +1,16 @@
 package dev.pinky.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
+//        val intent = Intent(this,LoginActivity::class.java)
+//        startActivity(intent)
+////        finish()
     }
 }
